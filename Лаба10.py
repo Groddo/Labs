1f64c3f27ea4c3433178b5b39b7b335897089694
Groddo <124997157+Groddo@users.noreply.github.com>
2024-02-29 16:32:35 +0400

    Update Лаба10.py

--- Лаба10.py
+++ Лаба10.py
@@ -6,7 +6,7 @@ from tkinter import *
 import random
 import copy
 
-turn = 'X'
+turn = 'X'                        #Начальные параметры
 p1 = random.choice(['X','O'])
 if p1 == 'X':
     p2 = 'O'
@@ -14,21 +14,21 @@ else:
     p2 = 'X'
 
 
-def click(r,c):
+def click(r,c):                #Функция обработки нажатия на клетку
     global xos
     global turn
     turn = 'X'
-    if p1 == 'X':
+    if p1 == 'X':                            #Если первый игрок играет за X
         btns[r][c].config(text='X', state=DISABLED)
         xos[r][c] = 'X'
         turn = 'O'
-        if not checkwin(xos, 'X'):
+        if not checkwin(xos, 'X'):        #Проверка победы первого игрока
             pcmove = pc()
             if pcmove:
                 a, b = pcmove[0], pcmove[1]
                 btns[a][b].config(text='O', state=DISABLED)
                 xos[a][b] = 'O'
-    elif p1 == 'O':
+    elif p1 == 'O':                        #Если первый игрок играет за O
         btns[r][c].config(text='O', state=DISABLED)
         xos[r][c] = 'O'
         turn = 'X'
@@ -40,40 +40,40 @@ def click(r,c):
 
     winner()
 
-def pc():
+def pc():                #Функция отвечающая за ходы компьютера
     posmove = []
     for row in range(3):
         for elem in range(3):
             if xos[row][elem] == '':
-                posmove.append([row, elem])
-    if posmove == []:
+                posmove.append([row, elem])      #Заполняем массив возможных ходов
+    if posmove == []:            #Проверяем на наличие ходов
         return
-    else:
+    else:                                #Проверяем, есть ли победа в 1 ход у одного из игроков
         for char in ['X', 'O']:
             for i in posmove:
                 xoscopy = copy.deepcopy(xos)
                 xoscopy[i[0]][i[1]] = char
                 if checkwin(xoscopy, char):
                     return i
-        for i in posmove:
+        for i in posmove:            #Захватываем центр, если он пустой
             if i == [1,1]:
                 return i
-        ugol = []
-        for i in posmove:
+        ugol = []    
+        for i in posmove:            #Захвтатываем пустые углы
             if i in [[0,0],[0,2],[2,0],[2,2]]:
                 ugol.append(i)
-        if ugol != []:
+        if ugol != []:                
             move = random.randint(0, len(ugol)-1)
             return ugol[move]
         storona = []
-        for i in posmove:
+        for i in posmove:            #Захватываем оставшиеся клетки
             if i in [[0,1],[1,0],[1,2],[2,1]]:
                 storona.append(i)
         if storona != []:
             move = random.randint(0, len(storona)-1)
             return storona[move]
 
-def checkwin(board, symv):
+def checkwin(board, symv):            #Проверяем, есть ли победитель
     for row in board:
         if row[0] == row[1] == row[2] == symv:
             return True
@@ -84,7 +84,7 @@ def checkwin(board, symv):
     else:
         return False
 
-def winner():
+def winner():            #Функция вывода результаты игры и запрета нажатия на оставшиеся кнопки
     if checkwin(xos, 'X'):
         for i in range(3):
             for j in range(3):
@@ -107,7 +107,7 @@ def winner():
 
 
 
-root = Tk()
+root = Tk()            #Создание основного окна
 root.title('Крестики-нолики')
 root.geometry('480x640')
 root.resizable(False, False)
@@ -121,22 +121,22 @@ player.pack(anchor='n', fill=X, expand=True)
 btns = [
         [0,0,0],
         [0,0,0],
-        [0,0,0]]
+        [0,0,0]]            #Подготовка массива для создания кнопок
 
 xos = [
     ['','',''],
     ['','',''],
-    ['','','']]
+    ['','','']]               #Массив, который будет использоваться для хранения X и O
 
 setka = xos
 
 
 for i in range(3):
     for j in range(3):
-        btns[i][j] = Button(root,text='',font='20', height=7,width=16, command=lambda r = i, c = j: click(r,c))
+        btns[i][j] = Button(root,text='',font='20', height=7,width=16, command=lambda r = i, c = j: click(r,c))        #Создаём игровое поле(кнопки)
         btns[i][j].place(x=0+160*j,y=0+140*i)
 
-if p2 == 'X':
+if p2 == 'X':                #Начало игры, если пользователь играет за O
     pcmove = pc()
     i, j = pcmove[0], pcmove[1]
     btns[i][j].config(text='X', state=DISABLED)
